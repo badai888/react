3fb3b6f91d62863f39b7b5c77e16a55210cba89c
曹高峰 <7785303+cao_gaofeng@user.noreply.gitee.com>
2020-09-16 23:44:50 +0800

    2

--- src/components/Todoconent.js
+++ src/components/Todoconent.js
@@ -3,7 +3,32 @@ export default class extends Component{
   constructor(){
   super();
   this.state ={
-    num:1
+    num:1,
+    ishow:true,
+    arr:[
+      {
+        name:'小明',
+        age:12,
+        gender:'女'
+      },
+      {
+        name:'小7',
+        age:18,
+        gender:'女'
+      },
+      {
+        name:'小9',
+        age:16,
+        gender:'男'
+      }
+    ],
+    richText:`
+    <div>
+      我是富文本
+      <h3>标题</h3>
+      <button>按钮</button>
+    </div>
+  `
   }
 }
 render(){
@@ -12,7 +37,42 @@ render(){
       <button onClick={(e)=>{this.hand(e,1)}}>
         11
       </button>
+      {
+        this.state.ishow
+        ?
+        <div>真的</div>
+        :
+        <div>假的</div>
+      }
+      <button onClick={
+         ()=>{this.setState({ishow: !this.state.ishow})}
+         }> 按钮</button>
+        {
+          this.state.arr.map((el,index)=>{
+            return (
+              <li key={index}>
+                <h1>{el.name}</h1>
+                <h2>{el.age}</h2>
+                <h3>{el.gender}</h3>
+              </li>
+            )
+          })
+        }
+        {
+          this.state.richText
+        }
+        <div dangerouslySetInnerHTML={{__html:this.state.richText}}></div>
       {this.state.num}
+      { 
+      this.state.arr.map((el,index)=>{
+        return (
+          <li key={index}>
+            {el.name}
+          </li>
+        )
+      })
+     
+      }
 
     </div>
   )
