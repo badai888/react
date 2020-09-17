4b38dd2a75be9bf64bbaefc483634409a00927e3
曹高峰 <7785303+cao_gaofeng@user.noreply.gitee.com>
2020-09-17 08:43:52 +0800

    1

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -6,13 +6,49 @@ export default class Todolist extends Component {
     super();
     this.state={
       isLike:true,
-      num:1
+      num:1,
+      arr:[
+        {
+          name:'小明',
+          age:12,
+          gender:'女'
+        },
+        {
+          name:'小7',
+          age:18,
+          gender:'女'
+        },
+        {
+          name:'小9',
+          age:16,
+          gender:'男'
+        }
+      ],
     }
     // this.hed =this.hed.bind(this)
   }
   render(){
     return(
       <div>
+        {
+          this.state.arr.map((el,index)=>{
+            return (
+            <button 
+            className={index===this.state.num?'boxe':''}
+            key={index}
+            onClick={()=>{this.setState({num:index})}}>{el.name}</button>
+            )
+          })
+        }
+        {
+          this.state.arr.map((el,index)=>{
+           return(
+           this.state.num=== index
+             &&
+            <div>{el.age}</div>
+           )
+          })
+        }
         <Todoheader/>
         <Con/>
         {/* <button onClick={()=>{this.setState({num:2})}} >111</button> */}
