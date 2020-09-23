a616eef38647717442560832185db06206e1808e
曹高峰 <7785303+cao_gaofeng@user.noreply.gitee.com>
2020-09-23 15:20:32 +0800

    4

--- src/App.js
+++ src/App.js
@@ -10,7 +10,43 @@ const App = ()=>{
    <Switch>
      <Route path="/login" component={Login}></Route>
      <Route path='/404' component={Fracb}></Route>
-    {
+     {
+       routes.map((route)=>{
+         return (
+           <Route
+           key={route.path}
+           path={route.path}
+           render={()=>{
+             return (
+                 <div>
+                   {
+                     (function(){
+                       if(route.meta.needlogin){
+                         let lod=localStorage.getItem('sks')
+                         if(lod){
+                           return (
+                             <route.component></route.component>
+                           )
+                         }else{
+                           return(
+                             <Redirect to="/login"></Redirect>
+                           )
+                         }
+                       }else{
+                         return(
+                           <route.component></route.component>
+                         )
+                       }
+                     }())
+                   }
+                 </div>
+             )
+           }}></Route>
+         
+         )
+       })
+     }
+    {/* {
       routes.map((route)=>{
         return (
           <Route
@@ -45,7 +81,7 @@ const App = ()=>{
           ></Route>
         )
       })
-    }
+    } */}
 
    </Switch>
     </>
